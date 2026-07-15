ebd6aab42785d5971ef1e85d65b12f199f4445b5
Kirill Pindyurin <kipindyurin@gmail.com>
2026-07-15 19:51:33 +0300

    [spi] added 'kp_' prefix for names belonging to the library

--- spi_driver/spi.c
+++ spi_driver/spi.c
@@ -6,23 +6,22 @@
  */
 
 #include "device_drivers/spi.h"
-#include "stm32f1xx_it.h"
 
 
 volatile uint8_t __gpioLayout = 0;
 
 
-void SPI_Resume() {
+void kp_SPI1_Resume() {
 	SPI1->CR1 |= SPI_CR1_SPE;
 }
 
 
-void SPI_Stop() {
+void kp_SPI1_Stop() {
 	SPI1->CR1 &= ~(SPI_CR1_SPE);
 }
 
 
-static void __Init_GPIO(const enum SPI_GPIO_Speed speed) {
+static void _Init_GPIO(const enum kp_SPI1_GPIO_Speed speed) {
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 	switch (__gpioLayout) {
@@ -32,17 +31,17 @@ static void __Init_GPIO(const enum SPI_GPIO_Speed speed) {
 	 * A5 - CLK  (Clock)       - Alternate output push\pull
 	 * A4 - NSS  (Chip Select) - General output PULL-UP
 	 */
-	case SPI_GPIO_LAYOUT_PRIM:
+	case kp_SPI1_GPIO_LAYOUT_PRIM:
 		// Reset pins
 		GPIOA->CRL &= ~(0xFFFFul << 16);
 		GPIOA->ODR &= ~(0xFul << 4);
 		// PULL-UP for A4
 		GPIOA->ODR |= (1ul << 4);
-		if (speed == SPI_GPIO_Speed_10MHz)
+		if (speed == kp_SPI1_GPIO_Speed_10MHz)
 			GPIOA->CRL |= (0x9491ul << 16);
-		else if (speed == SPI_GPIO_Speed_2MHz)
+		else if (speed == kp_SPI1_GPIO_Speed_2MHz)
 			GPIOA->CRL |= (0xA4A2ul << 16);
-		else if (speed == SPI_GPIO_Speed_50MHz)
+		else if (speed == kp_SPI1_GPIO_Speed_50MHz)
 			GPIOA->CRL |= (0xB4B3ul << 16);
 		break;
 	/* [Secondary layout]
@@ -51,7 +50,7 @@ static void __Init_GPIO(const enum SPI_GPIO_Speed speed) {
 	 * B3  - CLK  (Clock)       - Alternate output push\pull
 	 * A15 - NSS  (Chip Select) - General output PULL-UP
 	 */
-	case SPI_GPIO_LAYOUT_SCND:
+	case kp_SPI1_GPIO_LAYOUT_SCND:
 		// Change AF layout
 		AFIO->MAPR |= AFIO_MAPR_SPI1_REMAP;
 		// Reset pins
@@ -59,15 +58,15 @@ static void __Init_GPIO(const enum SPI_GPIO_Speed speed) {
 		GPIOA->CRH &= ~(0xFul << 28);
 		// General output PULL-UP for A15
 		GPIOA->ODR |= (1ul << 15);
-		if (speed == SPI_GPIO_Speed_10MHz) {
+		if (speed == kp_SPI1_GPIO_Speed_10MHz) {
 			 // A15
 			GPIOA->CRH |= (0x1ul << 28);
 			// B5..3
 			GPIOB->CRL |= (0x949ul << 8);
-		} else if (speed == SPI_GPIO_Speed_2MHz) {
+		} else if (speed == kp_SPI1_GPIO_Speed_2MHz) {
 			GPIOA->CRL |= (0x2ul << 28);
 			GPIOB->CRL |= (0xA4Aul << 8);
-		} else if (speed == SPI_GPIO_Speed_50MHz) {
+		} else if (speed == kp_SPI1_GPIO_Speed_50MHz) {
 			GPIOA->CRL |= (0x3ul << 28);
 			GPIOB->CRL |= (0xB4Bul << 8);
 		}
@@ -76,17 +75,17 @@ static void __Init_GPIO(const enum SPI_GPIO_Speed speed) {
 }
 
 
-void SPI_Init(const uint8_t gpioLayout,
-			  const enum SPI_GPIO_Speed gpioSpeed,
+void kp_SPI1_Init(const uint8_t gpioLayout,
+			  const enum kp_SPI1_GPIO_Speed gpioSpeed,
 			  const uint8_t endianness,
-			  const enum SPI_BAUDRATE_SCALER BRScaler) {
+			  const enum kp_SPI1_BAUDRATE_SCALER BRScaler) {
 	// To read latch pin later in SPI_SendBytes
 	__gpioLayout = gpioLayout;
 	// Enable clocking for SPI1
 	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-	__Init_GPIO(gpioSpeed);
+	_Init_GPIO(gpioSpeed);
 	// To prevent troubles with multiple SPI_Init() calls
-	SPI_Stop();
+	kp_SPI1_Stop();
 	// Setup
 	SPI1->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_MSTR;
 	SPI1->CR1 |= endianness;
@@ -96,11 +95,11 @@ void SPI_Init(const uint8_t gpioLayout,
 }
 
 
-void SPI_SendBytes_1(uint8_t data) {
+void kp_SPI1_SendBytes_1(const uint8_t data) {
 	  // CS to LOW
-	if (__gpioLayout == SPI_GPIO_LAYOUT_PRIM)
+	if (__gpioLayout == kp_SPI1_GPIO_LAYOUT_PRIM)
 		GPIOA->BRR = (1ul << 4);
-	else if (__gpioLayout == SPI_GPIO_LAYOUT_SCND)
+	else if (__gpioLayout == kp_SPI1_GPIO_LAYOUT_SCND)
 		GPIOA->BRR = (1ul << 15);
 	// wait until last operation isn't completed
 	while (!(SPI1->SR & SPI_SR_TXE));
@@ -109,8 +108,8 @@ void SPI_SendBytes_1(uint8_t data) {
 	// wait until our transmission is going
 	while (SPI1->SR & SPI_SR_BSY);
 	// CS to HIGH
-	if (__gpioLayout == SPI_GPIO_LAYOUT_PRIM)
+	if (__gpioLayout == kp_SPI1_GPIO_LAYOUT_PRIM)
 		GPIOA->BSRR = (1ul << 4);
-	else if (__gpioLayout == SPI_GPIO_LAYOUT_SCND)
+	else if (__gpioLayout == kp_SPI1_GPIO_LAYOUT_SCND)
 		GPIOA->BSRR = (1ul << 15);
 }
--- spi_driver/spi.h
+++ spi_driver/spi.h
@@ -1,59 +1,59 @@
 /*
- * spi.h
+ * kp_SPI1.h
  *
  *  Created on: 12 июл. 2026 г.
  *      Author: kipin
  */
 
-#ifndef INC_DEVICE_DRIVERS_SPI_H_
-#define INC_DEVICE_DRIVERS_SPI_H_
+#ifndef INC_DEVICE_DRIVERS_KP_SPI1_H
+#define INC_DEVICE_DRIVERS_KP_SPI1_H
 
 #include "stm32f101xb.h"
 #include "stdint.h"
 
-#define SPI_LAYOUT_PRIMARY 0x00
-#define SPI_LAYOUT_SECONDARY 0x01
+#define kp_SPI1_LAYOUT_PRIMARY 0x00
+#define kp_SPI1_LAYOUT_SECONDARY 0x01
 
 #define BR_CHUNK_BEG 3
 
-#define SPI_GPIO_LAYOUT_PRIM 0x00
-#define SPI_GPIO_LAYOUT_SCND 0x01
+#define kp_SPI1_GPIO_LAYOUT_PRIM 0x00
+#define kp_SPI1_GPIO_LAYOUT_SCND 0x01
 
-#define SPI_ENDIANNESS_MSB_FIRST   0x00
-#define SPI_ENDIANNESS_LSB_FIRST   SPI_CR1_LSBFIRST
+#define kp_SPI1_ENDIANNESS_MSB_FIRST   0x00
+#define kp_SPI1_ENDIANNESS_LSB_FIRST   kp_SPI1_CR1_LSBFIRST
 
 
-enum SPI_GPIO_Speed {
-	SPI_GPIO_Speed_10MHz = 0,
-	SPI_GPIO_Speed_2MHz,
-	SPI_GPIO_Speed_50MHz
+enum kp_SPI1_GPIO_Speed {
+	kp_SPI1_GPIO_Speed_10MHz = 0,
+	kp_SPI1_GPIO_Speed_2MHz,
+	kp_SPI1_GPIO_Speed_50MHz
 };
 
-enum SPI_BAUDRATE_SCALER {
-	SPI_BAUDRATE_SCALER_2 = 0,
-	SPI_BAUDRATE_SCALER_4,
-	SPI_BAUDRATE_SCALER_8,
-	SPI_BAUDRATE_SCALER_16,
-	SPI_BAUDRATE_SCALER_32,
-	SPI_BAUDRATE_SCALER_64,
-	SPI_BAUDRATE_SCALER_128,
-	SPI_BAUDRATE_SCALER_256,
+enum kp_SPI1_BAUDRATE_SCALER {
+	kp_SPI1_BAUDRATE_SCALER_2 = 0,
+	kp_SPI1_BAUDRATE_SCALER_4,
+	kp_SPI1_BAUDRATE_SCALER_8,
+	kp_SPI1_BAUDRATE_SCALER_16,
+	kp_SPI1_BAUDRATE_SCALER_32,
+	kp_SPI1_BAUDRATE_SCALER_64,
+	kp_SPI1_BAUDRATE_SCALER_128,
+	kp_SPI1_BAUDRATE_SCALER_256,
 };
 
 
-void SPI_Init(const uint8_t gpioLayout,
-			  const enum SPI_GPIO_Speed gpioSpeed,
+void kp_SPI1_Init(const uint8_t gpioLayout,
+			  const enum kp_SPI1_GPIO_Speed gpioSpeed,
 			  const uint8_t endianness,
-			  const enum SPI_BAUDRATE_SCALER BRScaler);
+			  const enum kp_SPI1_BAUDRATE_SCALER BRScaler);
 
 
-void SPI_Stop();
+void kp_SPI1_Stop();
 
 
-void SPI_Resume();
+void kp_SPI1_Resume();
 
 
-void SPI_SendBytes_1(const uint8_t data);
+void kp_SPI1_SendBytes_1(const uint8_t data);
 
 
-#endif /* INC_DEVICE_DRIVERS_SPI_H_ */
+#endif /* INC_DEVICE_DRIVERS_KP_SPI1 */
